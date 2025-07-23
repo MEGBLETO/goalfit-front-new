5497d5af4adf89142f6afbaa5fb732a17ccdbae3
MEGBLETO <carnelilordwiz@gmail.com>
2025-07-23 15:46:22 +0200

    added unsplash to next.config

--- next.config.js
+++ next.config.js
@@ -1,32 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, 
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatar.iran.liara.run',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.dribbble.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn.dribbble.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'v2.exercisedb.io',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "v2.exercisedb.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
 };
 
-
-
-module.exports = nextConfig; 
+module.exports = nextConfig;
